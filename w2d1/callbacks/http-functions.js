1e35ea9a49f8ba7beebbdc4ac1a64287bdc846a4
EDWARD YANG <eddiecody888@gmail.com>
2018-12-04 01:17:35 +0000

    lowercase uppercase reverse files added

---
+++ w2d1/callbacks/http-functions.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+module.exports = function getHTML (options, callback) {
+    /* Your code here */
+
+  var result = '';
+  var https = require('https');
+
+
+  /* Add your code here */
+  https.get(options, function (response) {
+
+     response.setEncoding('utf8');
+
+     response.on('data', function (data) {
+     result += data ;
+
+     });
+
+     response.on('end', function() {
+        console.log('Response stream complete.');
+
+        callback(result);
+     });
+
+   });
+
+};
+
+
+
